86f8ac9d990aed628e5556b558ac2551b5e38a24
gizmore <gizmore@wechall.net>
2022-08-18 03:36:45 +0200

    '7.0.1-r1520 tests'

--- Method/SetCookie.php
+++ Method/SetCookie.php
@@ -2,22 +2,32 @@
 namespace GDO\CORS\Method;
 
 use GDO\Core\Method;
+use GDO\Core\Application;
 
 /**
  * Cookie setting triggered by a web-bug.
  * 
  * @author gizmore
+ * @version 7.0.1
  * @since 6.11.3
+ * @deprecated Still needed?
  */
 final class SetCookie extends Method
 {
 	public function execute()
 	{
-		$path = $this->filePath('img/pixel.png');
+		$path = $this->getModule()->filePath('img/pixel.png');
 		$picture = file_get_contents($path);
 		hdr('Content-Type: image/png');
 		hdr('Content-Size: ' . strlen($picture));
-		die($picture);
+		$app = Application::$INSTANCE;
+		$app->timingHeader();
+
+		# out
+		if (!$app->isUnitTests())
+		{
+			die($picture);
+		}
 	}
 	
 }
--- lang/cors_en.php
+++ lang/cors_en.php
@@ -3,6 +3,7 @@ namespace GDO\CORS\lang;
 return [
     'cfg_cors_allow_any' => 'Allow any domain?',
     'cfg_cors_allow_creds' => 'Allow credentials to be exchanged?',
-	
+
+	'mt_cors_setcookie' => 'Webcookie',
 	'msg_cors_cookie_set' => 'Your cross domain cookie should have been set.',
 ];
