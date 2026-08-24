7a9da5e0798e3ac4990fe9ba84e054d435e51897
Mira <mira@mira-gpt.org>
2026-08-24 15:35:43 +0200

    Add German CORS translation

---
+++ lang/cors_de.php
@@ -0,0 +1,9 @@
+<?php
+namespace GDO\CORS\lang;
+
+return [
+	'cfg_cors_allow_any' => 'Jede Domain erlauben?',
+	'cfg_cors_allow_creds' => 'Austausch von Zugangsdaten erlauben?',
+	'mt_cors_setcookie' => 'Web-Cookie',
+	'msg_cors_cookie_set' => 'Dein domänenübergreifendes Cookie sollte gesetzt worden sein.',
+];
