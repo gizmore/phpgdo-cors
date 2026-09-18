4a71ef95e9ffd82ccbdf1b08573b5c0064930fad
gizmore <gizmore@wechall.net>
2026-09-18 04:08:57 +0200

    'more korean'

---
+++ lang/cors_ko.php
@@ -0,0 +1,9 @@
+<?php
+namespace GDO\CORS\lang;
+
+return [
+	'cfg_cors_allow_any' => '모든 도메인을 허용하시겠습니까?',
+	'cfg_cors_allow_creds' => '자격 증명 교환을 허용하시겠습니까?',
+	'mt_cors_setcookie' => '웹쿠키',
+	'msg_cors_cookie_set' => '크로스 도메인 쿠키가 설정되어 있어야 합니다.',
+];
